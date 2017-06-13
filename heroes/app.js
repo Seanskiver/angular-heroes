db8048c54cd7fc26594ce6b827ffe9f456feb2c4
Sean Skiver <seanskiver@gmail.com>
2017-06-13 03:30:24 +0000

    Starting a quick tutorial that will help me implement Node and Angular into the project

--- heroes/app.js
+++ heroes/app.js
@@ -2,10 +2,14 @@ var express = require('express');
 var http = require('http');
 var app = express();
 
-app.use(express.static(__dirname + '/public'));
+// ROUTES
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+// app.use(express.static(__dirname + '/public'));
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 
-
-
+app.use('/', index);
+app.use('/users', users);
 
 http.createServer(app).listen(8080);
